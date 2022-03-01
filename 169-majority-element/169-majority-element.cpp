4170bc729e2560a245612df1ebaa423d78468957
Dhruv <91935072+dhruvdabhi06@users.noreply.github.com>
2022-03-01 16:48:00 +0530

    Time: 24 ms (62.44%), Space: 19.6 MB (52.67%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int maxele ;
+        int count = 0;
+        for(int i=0;i<nums.size();i++){
+            if(count == 0){
+                maxele = nums[i];
+            }
+            if(nums[i] == maxele){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return maxele;
+    }
+};
