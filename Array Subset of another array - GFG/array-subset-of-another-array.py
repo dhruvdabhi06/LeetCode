4100cf5e99987673c9342965b70b96eb91c9d09e
Dhruv <91935072+dhruvdabhi101@users.noreply.github.com>
2022-05-02 00:01:42 +0530

    Added solution - LeetHub

---
+++ Array Subset of another array - GFG/array-subset-of-another-array.py
@@ -0,0 +1,40 @@
+#User function Template for python3
+
+def isSubset( a1, a2, n, m):
+    count = 0
+    for i in range(m):
+        if a2[i] in a1:
+            count = count + 1
+        else:
+            return "No"
+    if count == m:
+        return "Yes"
+    
+    
+    
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        sz = [int(x) for x in input().strip().split()]
+        n, m = sz[0], sz[1]
+        a1 = [int(x) for x in input().strip().split()]
+        a2 = [int(x) for x in input().strip().split()]
+        
+        print(isSubset( a1, a2, n, m))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
