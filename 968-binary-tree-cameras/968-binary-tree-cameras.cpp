c63625bc0bc96c21e6a284b974d3d4ec721133b8
Dhruv <91935072+dhruvdabhi101@users.noreply.github.com>
2022-06-17 09:12:15 +0530

    Time: 11 ms (83.13%), Space: 21.4 MB (23.37%) - LeetHub

---
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans = 0;
+    int minCameraCover(TreeNode* root) {
+        return (dfs(root) == "notCovered" ? 1 : 0) + ans;
+    }
+    string dfs(TreeNode *root){
+        if(root == NULL) return "covered";
+        string left = dfs(root->left);
+        string right = dfs(root->right);
+        if(left == "notCovered" || right == "notCovered"){
+            ans++;
+            return "havingCamera";
+        }
+        return left == "havingCamera" || right == "havingCamera" ? "covered" : "notCovered";
+    }
+};
