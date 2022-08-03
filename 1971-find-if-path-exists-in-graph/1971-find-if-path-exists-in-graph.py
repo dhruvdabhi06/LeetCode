ea0c6978a574124b8c50614208fab9c198da4ae8
Dhruv <91935072+dhruvdabhi101@users.noreply.github.com>
2022-08-04 00:41:13 +0530

    Time: 3086 ms (45.10%), Space: 106.5 MB (42.35%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -0,0 +1,20 @@
+
+
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        G = defaultdict(list[int])
+        for s , e in edges:
+            G[s].append(e)
+            G[e].append(s)
+        q = deque([source])
+        seen = set()
+        while q:
+            c = q.popleft()
+            if c == destination:
+                return True
+            if c in seen:
+                continue
+            seen.add(c)
+            for i in G[c]:
+                q.append(i)
+        return False
